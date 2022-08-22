967455b4d7d0f1405816b15240536ac1a07ac72d
Ronaldo Rios Espíndola <ronaldoriosespindola@gmail.com>
2022-08-22 12:45:24 -0300

    Ajustes Finais

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelector('.busca').addEventListener('.submit', async(event)=>{
+document.querySelector('.busca').addEventListener('submit', async (event)=>{
     event.preventDefault()
 
     //armazenar a informação digitada:
@@ -7,17 +7,17 @@ document.querySelector('.busca').addEventListener('.submit', async(event)=>{
     if(input !== ''){
         showWarning('Carregando...')
         // requisição pela api:
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encondeURI(input)}&appid=cb3cd9938feff1e6e485f19c6f7ffc56&units=metric&leng=pt_br`
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=cb3cd9938feff1e6e485f19c6f7ffc56&units=metric&lang=pt_br`;
         let results = await fetch(url)
         let json = await results.json()
-
+        
         if(json.cod === 200){
             clearInfo()
             showInfo({
                 name: json.name,
                 country: json.sys.country,
                 temp: json.main.temp,
-                tempIcon: icon.weather.icon,
+                tempIcon: json.weather[0].icon,
                 windSpeed: json.wind.speed,
                 windAngle: json.wind.deg
             })
@@ -41,7 +41,7 @@ function showInfo(json){
     document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`
     document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`
     document.querySelector('ventoInfo').innerHTML = `${json.windSpeed} <span> km/h </span>`
-    document.querySelector('. temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
+    document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
     document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle - 90}deg)`
 }
 
